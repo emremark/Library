e72544780a90201e000928025f0fbe87584e4887
emremark <mark.radicc@gmail.com>
2024-12-09 20:45:19 +0100

    Added basic form logic
    Form inputs on clicking Submit button create a
    new object and add it to the array.

--- scr.js
+++ scr.js
@@ -15,11 +15,38 @@ const myLib = [];
 
 function addToLib(Book) {
     myLib.push(Book)
-    
+    debugger
 }
-
+//open form and close form 
 const dial = document.getElementById("book_dialog");
 const newBook = document.getElementById("add");
 newBook.addEventListener("click", () => {
     dial.showModal();
+})
+const cls = document.getElementById("close")
+cls.addEventListener("click", () => {
+    dial.close();
+})
+
+//Create a new object
+function makeBook(t,a,n,r) {
+    const bk = new Book(t,a,n,r)
+    return bk
+    debugger
+}
+
+//get input from the form, make a object and add to the array
+const sub_btn = document.getElementById("submit")
+sub_btn.addEventListener("click", () => {
+    let tit = document.getElementById("title").value;
+    let aut = document.getElementById("author").value;
+    let nu = document.getElementById("pages").value;
+    let rd = document.getElementById("read").value;
+    const nBk = makeBook(tit,aut,nu,rd);
+    debugger
+    addToLib(nBk);
+    debugger
+    for (let b of myLib) {
+        console.log(b.info());
+    }
 })
