72f9278bcd634e987904788e3dfc0b36cce3a766
emremark <mark.radicc@gmail.com>
2024-12-12 18:15:51 +0100

    Added js for read checkbox to change object
    Checkbox read now changes the book.read object and
    displays properly. Everything now works, CSS left
    and a checkbox on the Book div to change Read
    status after adding a Book.

--- scr.js
+++ scr.js
@@ -4,12 +4,12 @@ function Book (title, author, n_pages, read) {
     this.n_pages = n_pages;
     this.read = read;
     this.info = function() {
-        return this.title + ' ' + 'by' + ' ' + this.author + ',' + ' ' + this.n_pages + ' ' + 'pages' + ',' + ' ' + this.read
+        return 'Title:' + ' ' + this.title + '<br>' +
+               'Author:' + ' ' + this.author + '<br>' + 
+               'Pages:' + ' ' + this.n_pages + '<br>' + 
+               this.read
     } 
 }
-
-//const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
-//console.log(theHobbit.info());
 
 const myLib = [];
 
@@ -34,60 +34,28 @@ function makeBook(t,a,n,r) {
     return bk
     
 }
-/////
+//Create a div for Book object, display it on page. Event listener to remove book button
 function makeDiv(argu) {
-    
-    //for (let o of myLib) {
-        //if (!myLib.includes(o)) {
             const newDiv = document.createElement("div");
+            const ndc = document.createElement("div");
+            ndc.className = "dcont";
             newDiv.className = "book";
-            newDiv.textContent = argu.info();
-            //newDiv.id = myLib.indexOf(o);
+            ndc.innerHTML = argu.info();
             const rmv = document.createElement("button");
             rmv.textContent = "X";
             rmv.className = "removing";
             const parent = document.getElementById("main");
+
             parent.appendChild(newDiv);
+            newDiv.appendChild(ndc);
             newDiv.appendChild(rmv);
+
             rmv.addEventListener("click", () => {
                 newDiv.remove();
                 let ind = myLib.indexOf(argu);
                 myLib.splice(ind, 1);
             })
-       // }
-       // else {
-            //nothing
-       // }
-//}
 }
-  
-
-//Create div, take object as arg and make div
-//function makeDiv(ob) {
-    
-    /*
-    const newDiv = document.createElement("div")
-    newDiv.className = "book";
-    newDiv.id = ob.title;
-    newDiv.textContent = ob.info();
-    const rmv = document.createElement("button")
-    rmv.textContent = "X"
-    const parent = document.getElementById("main");
-    parent.appendChild(newDiv);
-    newDiv.appendChild(rmv)
-    
-}
-*/
-//Remove book div when X button is pushed
-/*
-const buttons_remove = document.querySelectorAll(".removing")
-buttons_remove.forEach(dugme => {
-    dugme.addEventListener("click", () => {
-        let par = dugme.parentElement;
-        par.remove();
-    })
-})
-*/
 
 //Get input from the form, call function to make a object, call function to create div.
 const sub_btn = document.getElementById("submit")
@@ -95,11 +63,11 @@ sub_btn.addEventListener("click", () => {
     let tit = document.getElementById("title").value;
     let aut = document.getElementById("author").value;
     let nu = document.getElementById("pages").value;
-    let rd = document.getElementById("read").value;
+    let rd = document.getElementById("read").checked ? "Read" : "Not Read";
     const nBk = makeBook(tit,aut,nu,rd);
     addToLib(nBk);
-    //debugger
     makeDiv(nBk);
-    //debugger
+    document.getElementById("forma").reset();
+    dial.close();
 })
 
